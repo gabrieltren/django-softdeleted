2721fef02af25ed2b6dc416b7f7b4392f5419dc2
Gabrieltren <gabriel.silveira1207@gmail.com>
2022-08-30 01:51:30 -0300

    heroku

--- main_site/settings.py
+++ main_site/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import django_on_heroku
+
 import dj_database_url
 from decouple import config
 
@@ -161,4 +161,3 @@ REST_FRAMEWORK = {
     ]
 }
 
-django_on_heroku.settings(locals())
